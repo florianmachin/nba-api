c75afe06dace57d04b1589e3fa4fcde0c576fab9
florianmachin <tiflo99@gmail.com>
2021-04-27 13:06:09 +0200

    routes actives

---
+++ src/app/teams/pages/teams-detail/teams-detail.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { Observable } from 'rxjs';
+import { Player } from 'src/app/core/models/player';
+import { Teams } from 'src/app/core/models/teams';
+import { PlayerService } from 'src/app/core/services/http/player.service';
+import { TeamsService } from 'src/app/core/services/http/teams.service';
+
+
+@Component({
+  selector: 'app-teams-detail',
+  templateUrl: './teams-detail.component.html',
+  styleUrls: ['./teams-detail.component.css']
+})
+export class TeamsDetailComponent implements OnInit {
+
+  teamsId: number;
+
+  teams$: Observable<Teams>;
+
+  players$: Observable<Player[]>;
+
+  constructor(private _activateRoute: ActivatedRoute,
+    private _playerService: PlayerService,
+    private _teamsService: TeamsService) { }
+
+  ngOnInit(): void {
+    this.teamsId = Number(this._activateRoute.snapshot.paramMap.get('id'));
+
+    if (this.teamsId) {
+      this.fetchData(this.teamsId);
+      this.fetchPlayer(this.teamsId);
+    }
+  }
+  fetchData(id: number): void {
+    this.teams$ = this._teamsService.getById(id);
+  }
+  fetchPlayer(Player: Player): void {
+    this.players$ = this._teamsService.getByTeams(Player.team);
+  }
+}
